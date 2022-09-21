61e369ef023e1535bb66d9b7a6b1076081eeb77a
rboehner-art <110072465+rboehner-art@users.noreply.github.com>
2022-09-21 16:29:55 -0600

    Java_092122

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+
+function askName(){
+
+    console.log(firstName);
+    var firstName = prompt ("What is your name?");
+    console.log(firstName);
+    alert("Hello, " + firstName + "! Thank you for visiting my Resume" );
+}
+
